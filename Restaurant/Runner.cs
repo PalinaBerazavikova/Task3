8662b4e9bbd1fc75ae99dc76aa96ab6944625fc6
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-09-22 21:00:15 +0300

    й

---
+++ Restaurant/Runner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            Menu currentMenu = new Restaurant.Menu();
+            Console.ReadKey();
+        }
+    }
+}
